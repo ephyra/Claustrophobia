bad5dc48ee8ed3e755f0570eabf23348d2f73c3d
DESKTOP\Joey <yeojoey@gmail.com>
2017-10-30 16:53:40 +0800

    Basic menus

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+
+	public GameObject buttonPanel;
+	public GameObject creditsPanel;
+	public GameObject instructionsPanel;
+
+	void Start () {
+		// Reset the UI
+		ShowButtonPanel ();
+		HideInstructionsPanel ();
+		HideCreditsPanel ();
+	}
+
+	public void ShowInstructionsPanel () {
+		instructionsPanel.SetActive (true);
+	}
+
+	public void HideInstructionsPanel () {
+		instructionsPanel.SetActive (false);
+	}
+
+	public void ShowButtonPanel () {
+		buttonPanel.SetActive (true);
+	}
+
+	public void HideButtonPanel () {
+		buttonPanel.SetActive (false);
+	}
+
+	public void ShowCreditsPanel () {
+		creditsPanel.SetActive (true);
+	}
+
+	public void HideCreditsPanel () {
+		creditsPanel.SetActive (false);
+	}
+
+
+	public void NextScene () {
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+	}
+
+}
